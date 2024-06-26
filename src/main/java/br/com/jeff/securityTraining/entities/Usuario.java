b04f9981301415880e3de879f43fa6d3f996a562
Jefferson De Andrade <jeffersonandrade1999@gmail.com>
2024-06-26 12:05:48 -0300

    FEAT: configurando o JWTToken service e implementado a classe userdetails na usuario

--- src/main/java/br/com/jeff/securityTraining/entities/Usuario.java
+++ src/main/java/br/com/jeff/securityTraining/entities/Usuario.java
@@ -1,5 +1,12 @@
 package br.com.jeff.securityTraining.entities;
 
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -10,7 +17,7 @@ import lombok.Data;
 @Data
 @Entity
 @Table(name="T_JWT_USUARIO")
-public class Usuario {
+public class Usuario implements UserDetails{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,6 +29,36 @@ public class Usuario {
     private String password;
 
     private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 
 
 }
